b94a1adb0a4517d3953003e39d3edfa5e255eacc
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-05 18:31:36 -0300

    cors config

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,8 @@ const walletsRouter = require("./routes/wallets");
 server.use(express.json());
 // server.use(cors());
 server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", process.env.URL_SERVER);
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
   res.header("Access-Control-Allow-Credentials", "true");
   res.header(
     "Access-Control-Allow-Headers",
