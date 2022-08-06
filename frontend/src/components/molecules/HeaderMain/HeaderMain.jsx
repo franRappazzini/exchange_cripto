1174f4f7121b1b435f6a3ce0721ac86415fdcea3
Francisco Rappazzini <fran_rappa@outlook.com>
2022-08-06 11:05:26 -0300

    burger menu in headerMain component

--- frontend/src/components/molecules/HeaderMain/HeaderMain.jsx
+++ frontend/src/components/molecules/HeaderMain/HeaderMain.jsx
@@ -1,31 +1,58 @@
 import "./HeaderMain.scss";
 
+import { Box, Button, SwipeableDrawer } from "@mui/material";
+import { Close, Menu } from "@mui/icons-material";
 import { Link, useNavigate } from "react-router-dom";
-
-import { Button } from "@mui/material";
-import React from "react";
+import React, { useState } from "react";
 
 function HeaderMain({ setOption }) {
+  const [burger, setBurger] = useState(false);
+
   const navigate = useNavigate();
+
+  function toggleDrawer(option) {
+    setBurger(option);
+  }
 
   return (
     <header className="main-header_component">
       <Link to="/">
         <h1>HeaderMain</h1>
       </Link>
-      <div className="btn_container">
-        <Button
-          variant="contained"
-          onClick={() => navigate("/sign-in", setOption("signUp"))}
-        >
-          REGISTRARSE
+
+      <div>
+        <Button onClick={() => toggleDrawer(true)}>
+          {burger ? <Close /> : <Menu />}
         </Button>
-        <Button
-          variant="outlined"
-          onClick={() => navigate("/sign-in", setOption("logIn"))}
+        <SwipeableDrawer
+          anchor="right"
+          open={burger}
+          onClose={() => toggleDrawer(false)}
+          onOpen={() => toggleDrawer(true)}
         >
-          INGRESAR
-        </Button>
+          <Box
+            sx={{
+              display: "flex",
+              flexDirection: "column",
+              minWidth: window.innerWidth < 821 ? "65vw" : "25vw",
+              p: "1rem",
+              gridGap: "0.5rem",
+            }}
+          >
+            <Button
+              variant="contained"
+              onClick={() => navigate("/sign-in", setOption("signUp"))}
+            >
+              REGISTRARSE
+            </Button>
+            <Button
+              variant="outlined"
+              onClick={() => navigate("/sign-in", setOption("logIn"))}
+            >
+              INGRESAR
+            </Button>
+          </Box>
+        </SwipeableDrawer>
       </div>
     </header>
   );
